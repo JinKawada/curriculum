74e343491b6edcfa3a3d3cf3a8cb2ab57c6c97bb
JinKawada <JinKawada@icloud.com>
2021-03-05 17:50:20 +0900

    課題提出テスト
    Task1_2.java　再提出

--- JavaStudy/1-2/Task1_2.java
+++ JavaStudy/1-2/Task1_2.java
@@ -13,7 +13,6 @@ public class Task1_2 {
     public static void main(String[] args) {
 
         // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
-              int[] arr = new int[3];
               String[] a = {"JAPAN,AMERICA,KOREA"};
         // ② ①で作成した配列の要素数を出力してください。
               System.out.println(arr.length);
@@ -24,7 +23,9 @@ public class Task1_2 {
          */
               String[] strArray = new String[3];
         // ④ ③で作成した配列の2番目の要素を出力しなさい。
+              strArray[0] = "りんご";
               strArray[1] = "もも";
+              strArray[2] = "ぶどう";
               System.out.println(strArray[1]);
         /* ⑤下記の処理について、何をしているのかコメントを記入してください。
          *    宣言と初期化を同時にしている
